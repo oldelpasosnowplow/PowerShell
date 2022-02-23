c8d501495893978c385b40bbf34490593bca9ebb
oldelpasosnowplow <49489316+oldelpasosnowplow@users.noreply.github.com>
2022-02-23 07:58:01 -0500

    Update ChangeManager.ps1

--- ChangeManager.ps1
+++ ChangeManager.ps1
@@ -1,3 +1,9 @@
+# Script Created by Oldelpasosnowplow 
+# On Tuesday, November 4th, 2018
+# This is completely open to any and all that want to use it
+# The following script was created because I needed a fast way to change a users manager 
+# in Active Directory when there is turn over in that position.
+
 $OldManager = 'OldManagerGoesHere'
 $NewManager = 'NewManagerGoesHere'
 
