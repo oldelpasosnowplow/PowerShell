41a7614e5f3b484e4874cfb408d8f68733992a6b
oldelpasosnowplow <49489316+oldelpasosnowplow@users.noreply.github.com>
2022-02-23 07:52:39 -0500

    Add files via upload

---
+++ VMReport.ps1
@@ -0,0 +1,16 @@
+﻿Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
+#Enter your vm credentials here
+$vmcredentials = "root@localos"
+#Enter your vCenter server name here
+$vmvcentername = "vCenterServer"
+#Enter where you want the report to be stored
+$reportpath = "C:\temp\vmreport.html"
+
+$cred = Get-Credential $vmcredentials
+Connect-VIServer $vmvcentername -cred $cred
+
+$vms = Get-VM -Name *
+
+$vms | ConvertTo-Html -Title "$vmvcentername Report" -PreContent "<H1> $vmvcentername Report</H1>" -Property Name, Notes, PowerState, Guest, NumCpu, CoresPerSocket, MemoryGB, VMHost, HardwareVersion  | Out-File $reportpath
+
+Start-Process $reportpath
